c640375e35b5ec6a774ad1776ab09c76ed101682
joaomalfatti <joao.cursos1210@gmail.com>
2022-03-06 19:47:42 -0400

    Primeira rota de GET e POST, configuramos também Insomnia

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,27 @@
-import express from "express";
+import express, { request, response } from "express";
 
 // @types/express
 const app = express();
+
+
+/**
+ *  GET    => Buscar uma informação, 'Busca qualquer coisa'
+ *  POST   => Inserir uma informação, 'criações qualquer coisa'
+ *  PUT    => Alterar uma informação, 'manipulação de qualquer dado já existente, qualquer coisa'
+ *  DELETE => Remover uma informação, 'remover um dado'
+ *  PATCH  => Alterar uma informação específica, 'alguma alteração específica'
+ */ 
+
+
+app.get("/teste", (request, response) => {
+    //Request => Entrando
+    //Response => Saindo
+    return response.send("Hello World")
+});
+
+app.post("/teste-post", (request, response) => {
+    return response.send("Hello World método POST")
+});
 
 // http://localhost:3000
 app.listen(3000, () => console.log("Server is running"));
