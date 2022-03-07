859a897f1eac39d3b1564051be28cd6920acac1c
joaomalfatti <joao.cursos1210@gmail.com>
2022-03-06 22:21:57 -0400

    Configuramos o BD, arrumamos typeorm, escolhermos qual SGBD

--- src/server.ts
+++ src/server.ts
@@ -1,27 +1,10 @@
+import "reflect-metadata";
 import express, { request, response } from "express";
 
+import "./database";
 // @types/express
 const app = express();
 
 
-/**
- *  GET    => Buscar uma informação, 'Busca qualquer coisa'
- *  POST   => Inserir uma informação, 'criações qualquer coisa'
- *  PUT    => Alterar uma informação, 'manipulação de qualquer dado já existente, qualquer coisa'
- *  DELETE => Remover uma informação, 'remover um dado'
- *  PATCH  => Alterar uma informação específica, 'alguma alteração específica'
- */ 
 
-
-app.get("/teste", (request, response) => {
-    //Request => Entrando
-    //Response => Saindo
-    return response.send("Hello World")
-});
-
-app.post("/teste-post", (request, response) => {
-    return response.send("Hello World método POST")
-});
-
-// http://localhost:3000
 app.listen(3000, () => console.log("Server is running"));
