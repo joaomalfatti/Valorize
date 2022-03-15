7df8e7bbe4c4635801da59db751aa1968c3160a9
joaomalfatti <joao.cursos1210@gmail.com>
2022-03-14 20:25:58 -0400

    Foi criado uma entidadae, baixei uma biblioteca uuid, fiz as configurações na tabela

---
+++ src/entities/User.ts
@@ -0,0 +1,31 @@
+import {Entity, PrimaryColumn, Column, CreateDateColumn, UpdateDateColumn} from "typeorm";
+import {  } from "uuid";
+
+@Entity("users")
+class User {
+
+  @PrimaryColumn()
+  readonly id: string;
+
+  @Column()
+  name: string;
+
+  @Column()
+  email: string;
+
+  @Column()
+  admin: boolean;
+
+  @CreateDateColumn()
+  created_at: Date;
+
+  @UpdateDateColumn()
+  updated_at: Date;
+
+};
+
+export {User};
+
+
+// Entitidade < - > ORM < - > BD
+
