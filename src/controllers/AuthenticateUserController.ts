23385b5af7f79014ee73228e4927170bf72326a9
joao malfatti <joao.cursos1210@gmail.com>
2022-03-23 22:08:47 -0400

    começando a mexer com authenticate controller

---
+++ src/controllers/AuthenticateUserController.ts
@@ -0,0 +1,12 @@
+import { Request, Response } from "express";
+
+
+class AuthenticateUserController {
+
+  async handle(request: Request, response: Response) {
+    const { } = request.body
+  }
+
+}
+
+export { AuthenticateUserController }
